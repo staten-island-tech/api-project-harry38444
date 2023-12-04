a00f279ee25fb1c44b553387a4224e3f24674c21
harry38444 <harry38444@gmail.com>
2023-12-04 10:21:27 -0500

    a
    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,13 +3,12 @@ async function getData(URL) {
 try {
     const response = await fetch(URL);
     if(response.status !=200) {
-        throw new Error (response.statysText);
+        throw new Error (response.statusText);
     }
     const data = await response.json();
     document.querySelector("h1").textContent = data.content;
 } catch (error) {
-    console.log(error, "Uh OH sphaghettios");
-    document.querySelector("h1").textContent = "Eroor"
-}
-}
+    console.log(error, "Uh OH sphaghettios💀");
+    document.querySelector("h1").textContent = "Error"
+}}
     getData(URL);
