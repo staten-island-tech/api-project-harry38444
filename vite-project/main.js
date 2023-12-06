9dfc481c59eeec3f4801aaf4030d25023dcc95c1
harry38444 <harry38444@gmail.com>
2023-12-06 10:21:16 -0500

    a
    aaa

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,14 +1,11 @@
-const URL = `https://api.quotable.io/random`;
-async function getData(URL) {
-try {
-    const response = await fetch(URL);
-    if(response.status !=200) {
-        throw new Error (response.statusText);
-    }
+const URL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker`;
+async function getData(URLs) {
+    const response = await fetch(URLs);
     const data = await response.json();
-    document.querySelector("h1").textContent = data.content;
-} catch (error) {
-    console.log(error, "Uh OH sphaghettios💀");
-    document.querySelector("h1").textContent = "Error"
-}}
-    getData(URL);
+    document.querySelector("img").src = data.image;
+} 
+    getData(URL); 
+
+
+
+
