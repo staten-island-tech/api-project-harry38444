539ac7e6e37e2848c486313812d8d4fb7518bfc1
harry38444 <harry38444@gmail.com>
2023-12-07 10:21:28 -0500

    a
    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,11 +1,19 @@
-const URL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Decode%20Talker`;
-async function getData(URLs) {
-    const response = await fetch(URLs);
-    const data = await response.json();
-    document.querySelector("img").src = data.image;
-} 
-    getData(URL); 
 
-
-
-
+    const URLs = `https://digimon-api.vercel.app/api/digimon`;
+    async function getData(URLs) {
+    try {
+        const response = await fetch(URLs);
+        if(response.status !=200) {
+            throw new Error (response.statusText);
+        }
+        const data = await response.json();
+      data.forEach((data)=> console.log(data.name));
+        document.querySelector("h1").textContent = data;
+        const array = data.name
+        console.log(array)
+    } catch (error) {
+        console.log(error, "Uh Oh Sphagettios 💀");
+        document.querySelector("h1").textContent = "Eroor 🤓🤓"
+    }
+    }
+    getData(URLs);
