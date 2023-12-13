5a23d3b695b02b583c15a5b88c24d8cff9f3d4ee
harry38444 <harry38444@gmail.com>
2023-12-13 10:21:09 -0500

    a
    a

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,6 @@
-
+const DOMSelectors = {
+    column: document.querySelector(".column"),
+}
     const URLs = `https://db.ygoprodeck.com/api/v7/cardinfo.php`;
     async function getData(URLs) {
     try {
@@ -7,13 +9,28 @@
             throw new Error (response.statusText);
         }
         const data = await response.json();
-      data.data.forEach((data)=> console.log(data.name));
+      data.data.forEach((data)=> console.log(data.data.name));
+      data.data.forEach((data)=> console.log(data.data.card_images.image_url))
+      data.data.forEach((data)=> console.log(data.data.card_prices.tcgplayer_price))
         document.querySelector("h1").textContent = data;
-        const array = data.name
+      
+        function insertCards(arr){
+            arr.data.forEach((data) => {
+                DOMSelectors.column.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="card">
+                        <h3 class = "name">${data.name}</h3>
+                        <img src="${data.card_images.image_url}" class="img">
+                        <h4>Price: ${data.card_prices.tcgplayer_price}</h4> 
+                    </div>`
+                )
+            });
+        }
+        
+        insertCards(data)
         console.log(array)
     } catch (error) {
         console.log(error, "Uh Oh Sphagettios 💀");
         document.querySelector("h1").textContent = "Error 🤓🤓"
-    }
-    }
+    }}
     getData(URLs);
