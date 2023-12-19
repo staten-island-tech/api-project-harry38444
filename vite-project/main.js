a40684399c10e2fff47ef81f9d1d68c4ccefeb8b
harry38444 <harry38444@gmail.com>
2023-12-19 10:20:35 -0500

    nuh uh
    :(

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
     column: document.querySelector(".column"),
 }
-    const URLs = `https://db.ygoprodeck.com/api/v7/cardinfo.php`;
+    const URLs = `https://pokeapi.co/api/v2/berry?offset=20&limit=20`;
     async function getData(URLs) {
     try {
         const response = await fetch(URLs);
@@ -9,28 +9,27 @@ const DOMSelectors = {
             throw new Error (response.statusText);
         }
         const data = await response.json();
-     // data.data.forEach((data)=> console.log(data.data.name));
-     // data.data.forEach((data)=> console.log(data.data.card_images.image_url))
-     // data.data.forEach((data)=> console.log(data.data.card_prices.tcgplayer_price))
-     //   document.querySelector("h1").textContent = data;
+
       
         function insertCards(arr){
             arr.forEach((data) => {
+                console.log(data)
                 DOMSelectors.column.insertAdjacentHTML(
                     "beforeend",
                     `<div class="card">
                         <h3 class = "name">${data.name}</h3>
-                        <img src="${data.card_images.image_url}" class="img">
-                        <h4>Price: ${data.card_prices.tcgplayer_price}</h4> 
+                        <img src="${data.url}" class="img">
+
                     </div>`
                 )
             });
         }
-        
-        insertCards(data.data)
+
+        insertCards(data.results)
         console.log(array)
     } catch (error) {
         console.log(error, "Uh Oh Sphagettios 💀");
         document.querySelector("h1").textContent = "Error 🤓🤓"
     }}
     getData(URLs);
+    
