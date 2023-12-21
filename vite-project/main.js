a70f95318f8aa11a73b2b2a63df04641e90f1c28
harry38444 <harry38444@gmail.com>
2023-12-21 09:59:51 -0500

    aa
    aaaa

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,35 +1,40 @@
 const DOMSelectors = {
     column: document.querySelector(".column"),
 }
-    const URLs = `https://api.disneyapi.dev/character`;
-    async function getData(URLs) {
-    try {
-        const response = await fetch(URLs);
-        if(response.status !=200) {
-            throw new Error (response.statusText);
-        }
-        const data = await response.json();
+function insertCards(arr){
+    arr.forEach((data) => {
+        DOMSelectors.column.insertAdjacentHTML(
+            "beforeend",
+            `<div class="card">
+                <h3 class = "name">${data.name}</h3>
+                <img src="${data.imageUrl}" class="img">
+ 
+            </div>`
+        )
+    });
+}
 
-      
-        function insertCards(arr){
-            arr.info.forEach((data) => {
-                console.log(data)
-                DOMSelectors.column.insertAdjacentHTML(
-                    "beforeend",
-                    `<div class="card">
-                        <h3 class = "name">${data.name}</h3>
-                        <img src="${data.imageUrl}" class="img">
 
-                    </div>`
-                )
-            });
-        }
 
-        insertCards(data)
-        console.log(data)
-    } catch (error) {
-        console.log(error, "Uh Oh Sphagettios 💀");
-        document.querySelector("h1").textContent = "Error 🤓🤓"
-    }}
-    getData(URLs);
+    const URL = `https://api.disneyapi.dev/character`;
+
+async function getData(URL) {
+try {
+    // requesting a response REST API's
+    const response = await fetch(URL);
+    if(response.status !=200) {
+        throw new Error (response.statusText);
+    }
     
+
+    const data = await response.json();
+    insertCards(data.data)
+
+    console.log(data)
+    document.querySelector("h1").textContent = data.content;
+} catch (error) {
+    console.log(error, "Uh OH sphaghettios");
+    document.querySelector("h1").textContent = "aVDnghj"
+}
+}
+    getData(URL);
