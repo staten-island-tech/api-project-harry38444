8afe71a1f50f87e874e721e83b413e890dcb42b4
harry38444 <harry38444@gmail.com>
2024-01-03 10:21:55 -0500

    sa
    aa

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const DOMSelectors = {
         column: document.querySelector(".column"),
         nextButton: document.querySelector(".next-button"),
+        previousButton: document.querySelector(".previous-button")
     }
     function clearfields(){
         DOMSelectors.column.innerHTML="";
@@ -20,27 +21,21 @@ function insertCards(arr){
         )
     });
 }
-
-
-    const URL = `https://api.disneyapi.dev/character`;
-
+const URL = `https://api.disneyapi.dev/character`
 async function getData(page) {
     const URL = `https://api.disneyapi.dev/character?page=${page}`;
-
 try {
-
     const response = await fetch(URL);
     if(response.status !=200) {
         throw new Error (response.statusText);
     }
     const data = await response.json();
     insertCards(data.data)
-
     console.log(data)
     document.querySelector("h1").textContent = data.content;
 } catch (error) {
-    console.log(error, "Uh OH sphaghettios");
-    document.querySelector("h1").textContent = "aVDnghj"
+    console.log(error, "Error. No page found, please wait a bit before trying again. If you tried to click previous page, there might have been no page before it. Please note that there are not an infinite amount of pages.");
+    document.querySelector("h1").textContent = "Error. No page found, please wait a bit before trying again. If you tried to click previous page, there might have been no page before it. Please note that there are not an infinite amount of pages."
 }
 }
 DOMSelectors.nextButton.addEventListener('click', () => {
@@ -48,6 +43,10 @@ DOMSelectors.nextButton.addEventListener('click', () => {
     clearfields();
     getData(currentPage);
 });
-
+DOMSelectors.previousButton.addEventListener('click', () => {
+    currentPage--;
+    clearfields();
+    getData(currentPage);
+});
 getData(currentPage);
 });
